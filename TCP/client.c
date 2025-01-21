04812c52dcae9a570560584433ca50a71c0b8ef3
Mowsoon <alban.munari@gmail.com>
2025-01-21 20:35:55 +0100

    init TCP

---
+++ TCP/client.c
@@ -0,0 +1,11 @@
+//
+// Created by alban on 21/01/2025.
+//
+//
+// Created by alban on 21/01/2025.
+//
+#include <stdio.h>
+
+int main() {
+    printf("Hello client\n");
+}
---
+++ TCP/server.c
@@ -0,0 +1,8 @@
+//
+// Created by alban on 21/01/2025.
+//
+#include <stdio.h>
+
+int main() {
+    printf("Hello server\n");
+}
