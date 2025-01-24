ee6b930676fd207e3ea5d722bd8a551b7dbbe591
Mowsoon <alban.munari@gmail.com>
2025-01-24 22:00:54 +0100

    initialize socket

--- TCP/server.c
+++ TCP/server.c
@@ -17,11 +17,25 @@ void init_winsock(void) {
     }
 }
 
+int init_socket(void) {
+    int socket_fd = socket(AF_INET, SOCK_STREAM, 0);
+    if (socket_fd == -1) {
+        #ifdef _WIN32
+            fprintf(stderr, "socket() failed with error: %d\n", WSAGetLastError());
+        #else
+            perror("socket");
+        #endif
+            exit(1);
+    }
+    return socket_fd;
+}
+
 int main() {
     #ifdef _WIN32
         init_winsock();
     #endif
 
+    int socket_fd = init_socket();
 
     #ifdef _WIN32
         WSACleanup();
