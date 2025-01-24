361d6b6f7172fa3ab94c0eb51a9143688907fb13
Mowsoon <alban.munari@gmail.com>
2025-01-24 21:39:11 +0100

    add libraries

--- TCP/server.c
+++ TCP/server.c
@@ -1,7 +1,13 @@
-//
-// Created by alban on 21/01/2025.
-//
 #include <stdio.h>
+
+#ifdef _WIN32 // Windows
+    #include <winsock2.h>
+    #include <ws2tcpip.h>
+#else //Linux
+    #include <netinet/in.h>
+    #include <sys/socket.h>
+#endif
+
 
 int main() {
     printf("Hello server\n");
