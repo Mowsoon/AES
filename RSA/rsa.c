26943c6cc4af7925486a8c59c4ae19b615f764e0
Mowsoon <alban.munari@gmail.com>
2025-01-25 18:49:06 +0100

    adding find_pq

--- RSA/rsa.c
+++ RSA/rsa.c
@@ -1,12 +1,15 @@
 #include "rsa.h"
 
 void init_random();
+void generate_random_prime();
+void find_pq();
 
 void generate_rsa_key(mpz_t e, mpz_t d, mpz_t n) {
     mpz_t p, q, phi;
     gmp_randstate_t state;
 
-    init_random();
+    init_random(state);
+    find_pq(p, q, state);
 }
 
 void init_random(gmp_randstate_t state) {
@@ -20,3 +23,8 @@ void generate_random_prime(mpz_t prime, gmp_randstate_t state) {
     mpz_nextprime(prime, prime);
 }
 
+void find_pq(mpz_t p, mpz_t q, gmp_randstate_t state) {
+    mpz_init(p); mpz_init(q);
+    generate_random_prime(p, state);
+    generate_random_prime(q, state);
+}
