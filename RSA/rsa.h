6aa30f43e83e0bdea5c581d92f5e4613170d314e
Mowsoon <alban.munari@gmail.com>
2025-01-25 18:38:51 +0100

    completing header

--- RSA/rsa.h
+++ RSA/rsa.h
@@ -10,4 +10,10 @@
 #define RAND_SIZE 1024
 #define E 65537
 
+void generate_rsa_key(mpz_t e, mpz_t d, mpz_t n);
+char * crypt_rsa(char *message, mpz_t e, mpz_t n);
+char * decrypt_rsa(char *message, mpz_t d, mpz_t n);
+
+
+
 #endif //RSA_H
