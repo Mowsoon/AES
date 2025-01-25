6dc062cafb61d219a2b0a54802f4378db7fcfa62
Mowsoon <alban.munari@gmail.com>
2025-01-25 18:44:43 +0100

    adding init_random

--- RSA/rsa.c
+++ RSA/rsa.c
@@ -1,8 +1,17 @@
 #include "rsa.h"
 
+void init_random();
 
 void generate_rsa_key(mpz_t e, mpz_t d, mpz_t n) {
     mpz_t p, q, phi;
     gmp_randstate_t state;
+
+    init_random();
+}
+
+void init_random(gmp_randstate_t state) {
+    gmp_randinit_default(state);
+    unsigned long seed = time(NULL);
+    gmp_randseed_ui(state, seed);
 }
 
